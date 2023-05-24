b50eeb3a9787a8b51333723a5c6ba929608f53ff
Shudhanshu Jaiswal <shudhanshu.jaiswal@freecharge.com>
2023-05-24 05:53:17 +0530

    okta implemention

---
+++ src/main/java/com/ecommerce/cloudGateway/model/AuthenticationResponse.java
@@ -0,0 +1,21 @@
+package com.ecommerce.cloudGateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private long expiresAt;
+    private Collection<String> authorityList;
+
+}
